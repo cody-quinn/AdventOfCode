9c91c80ebf095bc24c030b493631b37681d076cb
Cody <cody@codyq.dev>
2024-11-30 22:08:26 -0600

    Improve solution for 2024 day 00 in Zig

--- 2024/zig/00/main.zig
+++ 2024/zig/00/main.zig
@@ -3,29 +3,23 @@ const std = @import("std");
 const input: []const u8 = @embedFile("input.txt");
 
 pub fn main() void {
-    var lines: u32 = 1;
+    var lines: u32 = 0;
     var palindromes: u32 = 0;
-    var word1: [256]u8 = undefined;
-    var word2: [256]u8 = undefined;
-    var i: u32 = 0;
-    for (input, 0..) |c, j| {
-        if (c == '\n') {
-            for (0..i) |k| {
-                if (std.ascii.toLower(word1[k]) != std.ascii.toLower(word2[256 - i + k])) {
-                    break;
-                }
-            } else {
-                palindromes += 1;
-            }
-            lines += 1;
-            i = 0;
-            continue;
-        }
 
-        word1[i] = input[j];
-        word2[255 - i] = input[j];
-        i += 1;
+    var iter = std.mem.splitScalar(u8, input, '\n');
+    while (iter.next()) |line| : (lines += 1) {
+        for (0..line.len) |i| {
+            const c1 = std.ascii.toLower(line[i]);
+            const c2 = std.ascii.toLower(line[line.len - i - 1]);
+
+            if (c1 != c2) {
+                break;
+            }
+        } else {
+            palindromes += 1;
+        }
     }
+
     std.debug.print("Part 1: {}\n", .{lines});
     std.debug.print("Part 2: {}\n", .{palindromes});
 }
