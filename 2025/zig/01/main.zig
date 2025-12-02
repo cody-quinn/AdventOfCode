2b7d2fb01bde4dab857a7276a1d4dc253e9ddeec
Cody <cody@codyq.dev>
2025-12-01 20:55:22 -0700

    AoC 2025 Day 01 in Zig

---
+++ 2025/zig/01/main.zig
@@ -0,0 +1,50 @@
+const std = @import("std");
+const mem = std.mem;
+
+pub fn main() !void {
+    const input: []const u8 = @embedFile("input.txt");
+    const part1, const part2 = problem(input);
+    std.debug.print("Part 1: {}\n", .{part1});
+    std.debug.print("Part 2: {}\n", .{part2});
+}
+
+fn problem(input: []const u8) !struct { u32, u32 } {
+    var part1: u32 = 0;
+    var part2: u32 = 0;
+    var dial: i32 = 50;
+
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+    while (lines.next()) |line| {
+        const sign: i32 = if (line[0] == 'L') -1 else 1;
+        const delta = try std.fmt.parseInt(u16, line[1..], 10);
+
+        for (0..delta) |_| {
+            dial += sign;
+
+            if (dial < 0) {
+                dial = 99;
+            } else if (dial > 99) {
+                dial = 0;
+            }
+
+            if (dial == 0) {
+                part2 += 1;
+            }
+        }
+
+        if (dial == 0) {
+            part1 += 1;
+        }
+    }
+
+    return .{ part1, part2 };
+}
+
+test "Sample Input" {
+    const input: []const u8 = @embedFile("input_sample.txt");
+    const part1, const part2 = try problem(input);
+    std.debug.print("Part 1: {}\n", .{part1});
+    std.debug.print("Part 2: {}\n", .{part2});
+    try std.testing.expectEqual(part1, 3);
+    try std.testing.expectEqual(part2, 6);
+}
