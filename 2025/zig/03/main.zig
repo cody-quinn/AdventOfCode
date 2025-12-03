ee3af00e240e66e2c919de8d5c9f49919e0630c3
Cody <cody@codyq.dev>
2025-12-02 23:17:39 -0700

    AoC 2025 Day 03 in Zig improved

--- 2025/zig/03/main.zig
+++ 2025/zig/03/main.zig
@@ -9,8 +9,6 @@ pub fn main() !void {
     std.debug.print("Part 2: {}\n", .{part2});
 }
 
-const Pair = struct { u32, usize };
-
 fn problem(input: []const u8) !struct { u64, u64 } {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -21,54 +19,42 @@ fn problem(input: []const u8) !struct { u64, u64 } {
 
     var lines = mem.tokenizeScalar(u8, input, '\n');
     while (lines.next()) |line| {
-        // Part 1
-        const largest, const pos = findLargestDigit(line[0..line.len - 1]);
-        const second, _ = findLargestDigit(line[pos + 1..]);
-        part1 += largest * 10 + second;
-
-        // Part 2
-        var buffer = try alloc.dupe(u8, line);
-        var length = buffer.len;
-        for (1..buffer.len) |i| {
-            const value = buffer[i] - '0';
-
-            var j: isize = @intCast(i - 1);
-            while (j >= 0 and length != 12) : (j -= 1) {
-                if (buffer[@intCast(j)] == ' ') {
-                    continue;
-                }
-
-                const prev = buffer[@intCast(j)] - '0';
-                if (prev >= value) {
-                    break;
-                }
-
-                buffer[@intCast(j)] = ' ';
-                length -= 1;
-            }
-        }
-
-        const output = try alloc.alloc(u8, line.len);
-        _ = mem.replace(u8, buffer, " ", "", output);
-        part2 += try std.fmt.parseInt(u64, output[0..12], 10);
+        part1 += try std.fmt.parseInt(u64, try findGreatestNumericalJoey(alloc, line, 2), 10);
+        part2 += try std.fmt.parseInt(u64, try findGreatestNumericalJoey(alloc, line, 12), 10);
     }
 
     return .{ part1, part2 };
 }
 
-fn findLargestDigit(slice: []const u8) Pair {
-    var largest: u32 = 0;
-    var pos: usize = 0;
+fn findGreatestNumericalJoey(alloc: mem.Allocator, slice: []const u8, target_length: usize) ![]u8 {
+    var buffer = try alloc.dupe(u8, slice);
+    defer alloc.free(buffer);
 
-    for (slice, 0..) |digit, i| {
-        const value: u32 = digit - '0';
-        if (value > largest) {
-            largest = value;
-            pos = i;
+    const output = try alloc.alloc(u8, slice.len);
+    errdefer alloc.free(output);
+
+    var length = buffer.len;
+    for (1..buffer.len) |i| {
+        const value = buffer[i] - '0';
+
+        var j: isize = @intCast(i - 1);
+        while (j >= 0 and length != target_length) : (j -= 1) {
+            if (buffer[@intCast(j)] == ' ') {
+                continue;
+            }
+
+            const prev = buffer[@intCast(j)] - '0';
+            if (prev >= value) {
+                break;
+            }
+
+            buffer[@intCast(j)] = ' ';
+            length -= 1;
         }
     }
 
-    return .{ largest, pos };
+    _ = mem.replace(u8, buffer, " ", "", output);
+    return output[0..target_length];
 }
 
 test "Sample Input" {
